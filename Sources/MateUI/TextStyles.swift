c86918889e64c19f66958a53f371ffe9b0cf12dc
Ugo Arangino <ugo@arangino.de>
2019-06-23 04:26:39 +0200

    Add TextStyles overview

---
+++ Sources/MateUI/TextStyles.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+extension Font.TextStyle: Identifiable {
+    public var id: Int {
+        return hashValue
+    }
+}
+
+public struct TextStyles: View {
+    private let all: [Font.TextStyle] = [
+        .largeTitle,
+        .title,
+        .headline,
+        .subheadline,
+        .body,
+        .callout,
+        .footnote,
+        .caption,
+    ]
+
+    public init() {}
+
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            ForEach(all) { style in
+                Text(String(describing: style))
+                    .font(.system(style))
+            }
+        }
+    }
+}
