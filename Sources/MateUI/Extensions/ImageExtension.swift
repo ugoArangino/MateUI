07bc50320cfb1a65d47e95389164b3853c5d2087
Ugo Arangino <ugo@arangino.de>
2020-03-12 10:15:48 +0100

    Add growing image

--- Sources/MateUI/Extensions/ImageExtension.swift
+++ Sources/MateUI/Extensions/ImageExtension.swift
@@ -7,4 +7,30 @@ public extension Image {
         }
         self.init(uiImage: uiImage)
     }
+
+    func growning(height: CGFloat? = 300) -> some View {
+        GeometryReader { (geometry: GeometryProxy) in
+            if geometry.frame(in: .global).minY <= 0 {
+                self
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(
+                        width: geometry.size.width,
+                        height: geometry.size.height
+                    )
+            } else {
+                self
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .offset(y: -geometry.frame(in: .global).minY)
+                    .frame(
+                        width: geometry.size.width,
+                        height: geometry.size.height
+                            + geometry.frame(in: .global).minY
+                    )
+            }
+        }
+        .background(Color.red)
+        .frame(maxHeight: height)
+    }
 }
